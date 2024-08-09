d8d0579e1de3c69612b9def92d1953acb195970a
inilay <indioraaa@gmail.com>
2024-08-09 20:36:39 +0300

    change port to 8000

--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -6,7 +6,7 @@ export const AuthContext = createContext(null)
 
 export const ThemeContext = createContext(null);
 
-const baseURL = "http://127.0.0.1:49088/api/v1";
+const baseURL = "http://127.0.0.1:8000/api/v1";
 
 export const AuthProvider = ({ children }) => {
     const [authTokens, setAuthTokens] = useState(() =>
