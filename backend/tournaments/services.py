b31345be2258bfd5974f9629fdac36282d97f4c4
inilay <indioraaa@gmail.com>
2024-09-03 23:52:43 +0300

    add rr alg

--- backend/tournaments/services.py
+++ backend/tournaments/services.py
@@ -57,6 +57,67 @@ def model_update(*, instance, fields: List[str], data: Dict[str, Any], auto_upda
 
     return instance, has_updated
 
+def create_rr_bracket(bracket: Bracket, participants: list):
+    participants_cnt = len(participants)
+    rounds = []
+    unsaved_matches = []
+    matches_info = []
+
+    # всегда дополняем до четного
+    if participants_cnt % 2 == 1: 
+        participants = participants + ['None'] 
+        # смешаем начало на 1
+        start = 1 
+    else:   
+        start = 0
+
+    permutations = list(range(participants_cnt))
+    mid = participants_cnt // 2 
+    match_serial_number_cnt = 0
+
+    # O(n)
+    for i in range(participants_cnt-1):
+        _round = rounds.append(Round(bracket=bracket, serial_number=i))
+        l1 = permutations[:mid]
+        l2 = permutations[mid:]
+        # O(n/2)
+        l2.reverse()
+        round = []
+        # O(n/2)
+        for j in range(start, mid):
+            match = Match(round=_round, serial_number=match_serial_number_cnt, result_id=1)
+            unsaved_matches.append(match)
+            t1 = participants[l1[j]]
+            t2 = participants[l2[j]]
+            if j == 0 and i % 2 == 1:
+                matches_info.append(MatchParticipantInfo(
+                        match=match,
+                        participant_scoore=0,
+                        participant=t2
+                    ))
+                matches_info.append(MatchParticipantInfo(
+                        match=match,
+                        participant_scoore=0,
+                        participant=t1
+                    ))
+            else:
+                matches_info.append(MatchParticipantInfo(
+                        match=match,
+                        participant_scoore=0,
+                        participant=t1
+                    ))
+                matches_info.append(MatchParticipantInfo(
+                        match=match,
+                        participant_scoore=0,
+                        participant=t2
+                    ))
+            match_serial_number_cnt = match_serial_number_cnt + 1
+
+        permutations = permutations[mid:-1] + permutations[:mid] + permutations[-1:]
+
+    Round.objects.bulk_create(rounds)
+    Match.objects.bulk_create(unsaved_matches)
+    MatchParticipantInfo.objects.bulk_create(matches_info)
 
 def create_se_bracket(bracket: Bracket, participants: list):
     # log(player_in_match)total_players -  total number of rounds in the tournament 
@@ -217,13 +278,18 @@ def create_tournament(*, title: str, content: str,  poster, game: str, prize: fl
 
     participants = clear_participants(participants)
 
-    # if bracket_type == 1:
-    #     bracket = Bracket.objects.create(tournament=tournament, bracket_type_id=1, participant_in_match=2)
-    #     create_se_bracket(bracket, participants)
-    # else:
-    bracket_w = Bracket.objects.create(tournament=tournament, bracket_type_id=2, participant_in_match=2)
-    bracket_l = Bracket.objects.create(tournament=tournament, bracket_type_id=3, participant_in_match=2)
-    create_de_bracket(bracket_w, bracket_l, participants)
+    if bracket_type == 1:
+        bracket = Bracket.objects.create(tournament=tournament, bracket_type_id=1, participant_in_match=2)
+        create_se_bracket(bracket, participants)
+    elif bracket_type == 4:
+        bracket = Bracket.objects.create(tournament=tournament, bracket_type_id=4, participant_in_match=2)
+        create_rr_bracket(bracket, participants)
+    elif bracket_type == 5:
+        bracket = Bracket.objects.create(tournament=tournament, bracket_type_id=5, participant_in_match=2)
+    else:
+        bracket_w = Bracket.objects.create(tournament=tournament, bracket_type_id=2, participant_in_match=2)
+        bracket_l = Bracket.objects.create(tournament=tournament, bracket_type_id=3, participant_in_match=2)
+        create_de_bracket(bracket_w, bracket_l, participants)
 
     # if tournament_type == True:
     #         multi_stage = MultiStage(clear_participants(participants),
