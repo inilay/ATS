e11e3465cbcbc67d708c1c85687396b9e071e3d2
inilay <indioraaa@gmail.com>
2025-04-10 13:26:44 +0300

    add static tables

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,8 @@ python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
 
+python manage.py loaddata static_table.json
+
 # Start server
 
 # python3 manage.py runserver 0.0.0.0:49088
